173eb82a393244c695e7c796bcd39847ae4fa89f
iLern <416138794@qq.com>
2019-04-26 08:23:30 +0800

    可以通过‘今天’按钮返回今天

--- src/GetDay.java
+++ src/GetDay.java
@@ -5,8 +5,8 @@ import java.util.Calendar;
 public class GetDay {
 	//This Day
 	static public Calendar now_c = Calendar.getInstance();
-	static public int now_year = now_c.get(Calendar.YEAR);
-	static public int now_month = now_c.get(Calendar.MONTH);
+	static public String now_year = "" + now_c.get(Calendar.YEAR);
+	static public String now_month = "" + (now_c.get(Calendar.MONTH) + 1);
 	
 	//week
 	static public String[] week = new String[] {"Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"};
@@ -15,7 +15,7 @@ public class GetDay {
 	static public int maxDay = 28; 
 	
 	static public void setYearAndMonth() {
-		for (int i = GetDay.now_year - 100; i < GetDay.now_year + 20; i++) {
+		for (int i = Integer.parseInt(now_year) - 100; i < Integer.parseInt(now_year) + 20; i++) {
 			Window.years.addItem("" + i);
 		}
 		Window.years.setSelectedIndex(100);
@@ -23,12 +23,13 @@ public class GetDay {
 		Window.top.add(Window.y_label);
 		
 		//add Date
-		Window.Ok.addActionListener(new clickListener());
+		Window.Ok.addActionListener(new okListener());
+		Window.Today.addActionListener(new todayListener());
 		
 		for (int i = 1; i <= 12; i++ ) {
 			Window.months.addItem("" + i);
 		}
-		Window.months.setSelectedIndex(GetDay.now_month);
+		Window.months.setSelectedIndex(Integer.parseInt(now_month));
 		Window.top.add(Window.months);
 		Window.top.add(Window.m_label);
 	}
@@ -52,10 +53,33 @@ public class GetDay {
 		else return false;
 	}
 	
+	static void printDay(String monthYouWant, String yearYouWant, String dirstDayOfThatWeek) {
+		int iMonthYouWant = Integer.parseInt(monthYouWant);
+		if (iMonthYouWant == 1 || iMonthYouWant == 3 || iMonthYouWant == 5 || iMonthYouWant == 7 || iMonthYouWant == 8 || 
+			iMonthYouWant == 10 || iMonthYouWant == 12) {
+			maxDay = 31;
+		} else if (iMonthYouWant == 2) {
+			if (!isLeapYear(Integer.parseInt(yearYouWant))) maxDay = 28;
+			else maxDay = 29;
+		} else maxDay = 30;
+		
+		int cnt = 1;
+		for (int i = 0; i < 42; i++) {
+			if (i < Integer.parseInt(firstDayOfThatWeek) - 1) {
+				Window.days[i].setText(" ");
+			} else if (cnt > maxDay){
+				Window.days[i].setText(" ");
+			} else {
+				Window.days[i].setText(" " + cnt);
+				cnt++;
+			}
+		}
+	}
+	
 	static public String yearYouWant;
 	static public String monthYouWant;
 	static public String firstDayOfThatWeek;
-	static class clickListener implements ActionListener {
+	static class okListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
@@ -65,26 +89,20 @@ public class GetDay {
 			firstDayOfThatWeek = "" + getDay(yearYouWant, monthYouWant);
 //			System.out.println(yearYouWant +  " " + monthYouWant + " " + firstDayOfThatWeek);
 			
-			int iMonthYouWant = Integer.parseInt(monthYouWant);
-			if (iMonthYouWant == 1 || iMonthYouWant == 3 || iMonthYouWant == 5 || iMonthYouWant == 7 || iMonthYouWant == 8 || 
-				iMonthYouWant == 10 || iMonthYouWant == 12) {
-				maxDay = 31;
-			} else if (iMonthYouWant == 2) {
-				if (!isLeapYear(Integer.parseInt(yearYouWant))) maxDay = 28;
-				else maxDay = 29;
-			} else maxDay = 30;
+			printDay(yearYouWant, monthYouWant, firstDayOfThatWeek);
+		}
+	}
+	
+	static class todayListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Window.years.setSelectedItem(now_year);
+			Window.months.setSelectedItem(now_month);
 			
-			int cnt = 1;
-			for (int i = 0; i < 42; i++) {
-				if (i < Integer.parseInt(firstDayOfThatWeek) - 1) {
-					Window.days[i].setText(" ");
-				} else if (cnt > maxDay){
-					Window.days[i].setText(" ");
-				} else {
-					Window.days[i].setText(" " + cnt);
-					cnt++;
-				}
-			}
+			firstDayOfThatWeek = "" + getDay(now_year, now_month);
+			
+			printDay(now_year, now_month, firstDayOfThatWeek);
 		}
 	}
 }
